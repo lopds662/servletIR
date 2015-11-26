07f881c03a33810fe422165afacb56cd78686da7
JOMPOL SERMSOOK <runelopds662@gmail.com>
2015-11-26 16:08:18 +0700

    New

--- src/configFile/part/DataFile.java
+++ src/configFile/part/DataFile.java
@@ -2,10 +2,11 @@ package configFile.part;
 
 public class DataFile {
 	
-	public String FOLDER_NAME_IN_DATA = "M data"; 
-	public String FOLDER_NAME_OUT_INDEX = "E:\\index-directory-for-M";
-	public String SEARCH_FOLDER_PATH = "E:\\index-directory-for-M";
-//	public String SEARCH_FOLDER_PATH = "F:\\EMPTY\\programming\\JAVA\\testServlet\\index-directory-for-M";
+	public String FOLDER_NAME_IN_DATA = "M data";
+	public String FOLDER_NAME_OUT_INDEX = "index-directory-for-M";
+//	public String FOLDER_NAME_OUT_INDEX = "E:\\index-directory-for-M";
+//	public String SEARCH_FOLDER_PATH = "E:\\index-directory-for-M";
+	public String SEARCH_FOLDER_PATH = "F:\\EMPTY\\programming\\JAVA\\testServlet\\index-directory-for-M";
 	
 	public DataFile(){
 		
